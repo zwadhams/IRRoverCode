6835d51215ab41f4e1f6f676b0d00976c20f550f
Zach Wadhams <33103466+zwadhams@users.noreply.github.com>
2021-12-05 12:20:02 -0700

    commented code

--- getIRCodes.ino
+++ getIRCodes.ino
@@ -1,23 +1,23 @@
 #include <IRremote.h>
 #include <IRremoteInt.h>
 
-#define IRPIN 7
+#define IRPIN 7 //pin that the ir reciever is connected to
 
 IRrecv irrecv(IRPIN);
 decode_results result;
 
 
-
+//starts up the ir reciever and prints status message
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   irrecv.enableIRIn();
   Serial.println("Enabled IRin");
 
 }
 
+//decodes the recieved signal and prints out the value returned
+//this is used to map the remote for rover movement
 void loop() {
-  // put your main code here, to run repeatedly:
   if (irrecv.decode(&result)) 
   {
     Serial.println(result.value);
